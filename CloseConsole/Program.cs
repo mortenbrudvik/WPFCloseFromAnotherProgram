7ebe14c82dec9c666dc2d93377ea10534f8f9255
Morten Brudvik <morten@brudvik.com>
2021-03-26 15:10:59 +0100

    Example closing WPF window by sending a WM_CLOSE message from another program.

---
+++ CloseConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using PInvoke;
+using static System.Console;
+
+namespace CloseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Testing closing WPFApp from Console");
+
+            var process = Process.Start("WPFApp.exe");
+            WaitWhile(() => process.MainWindowHandle == IntPtr.Zero);
+            var windowHandle = process.MainWindowHandle;
+            WriteLine($"Press key to close WPF window (Handle: {windowHandle})");
+            ReadKey();
+            WriteLine("Closing WPF window");
+
+            User32.PostMessage(windowHandle, User32.WindowMessage.WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+
+
+        }
+
+        public static void WaitWhile(Func<bool> waitWhile, int timeoutInSeconds = 2, int sleepIntervalInMs = 100)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            while ( waitWhile() && watch.ElapsedMilliseconds < timeoutInSeconds*1000)
+            {
+                Thread.Sleep(sleepIntervalInMs);
+            }
+        }
+    }
+}
