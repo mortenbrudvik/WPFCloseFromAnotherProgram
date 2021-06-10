2bad2bbb54363b3ea64d11f7670685afc0cb5073
Morten Brudvik <morten@brudvik.com>
2021-06-10 15:04:41 +0200

    Added Custom TitleBar to be able to override the behavior of the close button.

--- WPFApp/MainViewModel.cs
+++ WPFApp/MainViewModel.cs
@@ -6,13 +6,16 @@ namespace WPFApp
     {
         private readonly Action _trayIconLeftButtonClicked;
         private readonly Action _quitApplicationRequested;
+        private readonly Action _hideWindowRequested;
 
-        public MainViewModel(Action trayIconLeftButtonClicked, Action quitApplicationRequested)
+        public MainViewModel(Action trayIconLeftButtonClicked, Action quitApplicationRequested, Action hideWindowRequested)
         {
             _trayIconLeftButtonClicked = trayIconLeftButtonClicked;
             _quitApplicationRequested = quitApplicationRequested;
+            _hideWindowRequested = hideWindowRequested;
         }
         public DelegateCommand ShowWindowCommand => new DelegateCommand(x => _trayIconLeftButtonClicked());
         public DelegateCommand QuitCommand => new DelegateCommand(x => _quitApplicationRequested());
+        public DelegateCommand HideWindowCommand => new DelegateCommand(x => _hideWindowRequested());
     }
 }
--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel;
-using System.Windows;
+﻿using System.Windows;
 
 namespace WPFApp
 {
@@ -11,42 +9,7 @@ namespace WPFApp
         {
             InitializeComponent();
 
-            DataContext = new MainViewModel(Show, Close);
+            DataContext = new MainViewModel(Show, Close, Hide);
         }
-
-        protected override void OnInitialized(EventArgs e)
-        {
-            base.OnInitialized(e);
-        }
-
-        // protected override void OnSourceInitialized(EventArgs e)
-        // {
-        //     base.OnSourceInitialized(e);
-        //     var source = PresentationSource.FromVisual(this) as HwndSource;
-        //     source.AddHook(WndProc);
-        // }
-        //
-        // static uint WM_CLOSE = 0x10;
-        // public const Int32 WM_SYSCOMMAND = 0x112;
-        // private static uint SC_CLOSE = 0xF060;
-        // private bool _wmCloseCalled;
-        // private bool _scCloseCalled;
-        //
-        // /// <summary>
-        // /// WM_CLOSE is sent as a window message whenever the window is requested to be closed, by any means.
-        // /// SC_CLOSE is sent as a parameter of a WM_SYSCOMMAND message, when the user presses the Close button (or selects Close from the control menu of the window).
-        // /// </summary>
-        // private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        // {
-        //     var isThisWindow = hwnd == Process.GetCurrentProcess().MainWindowHandle;
-        //
-        //     if (msg == WM_CLOSE && isThisWindow)
-        //         _wmCloseCalled = true;
-        //     
-        //     if (msg == WM_SYSCOMMAND && wParam.ToInt32() == SC_CLOSE && isThisWindow)
-        //         _scCloseCalled = true;
-        //     
-        //     return IntPtr.Zero;
-        // }
     }
 }
