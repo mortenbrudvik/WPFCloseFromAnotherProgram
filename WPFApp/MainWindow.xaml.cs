2f46ab7d50aa0ea076f2307c5c2a47db5c462e4e
Morten Brudvik <morten@brudvik.com>
2021-06-10 23:36:11 +0200

    added mahapps framework, added custom balloon message view.

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -1,15 +1,34 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
 
 namespace WPFApp
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = new MainViewModel(Show, Close, Hide);
+        }
+
+        protected override async void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            Hide();
+            
+            var notificationView = new NotificationView()
+            {
+                Title = "WPFApp",
+                Message = "The application window has been minimized to tray."
+            };
+
+            TaskbarIcon.ShowCustomBalloon(notificationView, PopupAnimation.Slide, 4000);
+
+            await Task.Delay(4000);
+            TaskbarIcon.CloseBalloon();
         }
     }
 }
---
+++ WPFApp/NotificationView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace WPFApp
+{
+    public partial class NotificationView
+    {
+        public NotificationView() => InitializeComponent();
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof (string), typeof (NotificationView), new FrameworkPropertyMetadata(string.Empty));
+
+        public string Title
+        {
+            get => (string) GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+        
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register(nameof(Message), typeof (string), typeof (NotificationView), new FrameworkPropertyMetadata(string.Empty));
+
+        public string Message
+        {
+            get => (string) GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+
+        private void grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var taskbarIcon = TaskbarIcon.GetParentTaskbarIcon(this);
+            taskbarIcon.ResetBalloonCloseTimer();
+        }
+    }
+}
